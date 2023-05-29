cfecdcf4eef58d39372c3393ddd907b6aa8e83ec
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-05-29 02:42:16 -0400

    adds db connection

--- next13/src/utils/database.ts
+++ next13/src/utils/database.ts
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 
-let isConnected = false;
+let isConnected: boolean = false;
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
@@ -10,11 +10,14 @@ export const connectToDB = async () => {
     return;
   }
   try {
-    mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "todos",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    mongoose.connect(
+      process.env.MONGODB_URI as string,
+      {
+        dbName: "todos",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions
+    );
     isConnected = true;
     console.log("MongoDB connected");
   } catch (error) {
