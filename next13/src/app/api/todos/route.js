0197f072faeff22f020686af3e01a4387f306c46
yashwanth-git <karthikyashwanth2009@gmail.com>
2023-06-01 03:10:35 -0400

    fixes response in get api

--- next13/src/app/api/todos/route.js
+++ next13/src/app/api/todos/route.js
@@ -4,8 +4,8 @@ import Todo from "@models/todo";
 export const GET = async (req) => {
   try {
     await connectToDB();
-    const todos = Todo.find({}).exec();
-    console.log(todos);
+    const todos = await Todo.find({}).exec();
+    return new Response(JSON.stringify(todos), { status: 200 });
   } catch (err) {
     console.log("Failed to fetch the todos", { status: 500 });
   }
